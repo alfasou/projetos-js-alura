f7e2f8b302fa4775513237103879964a7ae51293
Andressa de Faria Souza <afariasouza@outlook.com>
2024-10-24 11:37:56 -0300

    Formatação dos documentos com Prettier

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,28 +1,30 @@
-let totalGeral;
-limpar();
+let totalGeral
+limpar()
 
 function adicionar() {
     //recuperar valores nome do produto, quantidade e valor
-    let produto = document.getElementById('produto').value;
-    let nomeProduto = produto.split('-')[0];
-    let valorUnitario = produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade').value; 
+    let produto = document.getElementById('produto').value
+    let nomeProduto = produto.split('-')[0]
+    let valorUnitario = produto.split('R$')[1]
+    let quantidade = document.getElementById('quantidade').value
     //calcular o preço, o nosso subtotal
-    let preco = quantidade * valorUnitario;
-    let carrinho = document.getElementById('lista-produtos');
+    let preco = quantidade * valorUnitario
+    let carrinho = document.getElementById('lista-produtos')
     //adicionar no carrinho
-    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    carrinho.innerHTML =
+        carrinho.innerHTML +
+        `<section class="carrinho__produtos__produto">
     <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R${preco}</span>
-  </section>`;
+  </section>`
     //atualizar o valor total
-    totalGeral = totalGeral + preco;
-    let campoTotal = document.getElementById('valor-total'); 
-    campoTotal.textContent =  `R$ ${totalGeral}`;
-    document.get.ElementById('quantidade').value = 0;
+    totalGeral = totalGeral + preco
+    let campoTotal = document.getElementById('valor-total')
+    campoTotal.textContent = `R$ ${totalGeral}`
+    document.get.ElementById('quantidade').value = 0
 }
 
 function limpar() {
-    totalGeral = 0;
-    document.getElementById('lista-produtos').innerHTML = '';
-    document.getElementById('valor-total').textContent = 'R$ 0';
+    totalGeral = 0
+    document.getElementById('lista-produtos').innerHTML = ''
+    document.getElementById('valor-total').textContent = 'R$ 0'
 }
